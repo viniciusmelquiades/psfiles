2deee5eeeec08878d0c4781080719329d306e9fc
Vinicius Salvati Melquiades <vinicius.melquiades@outlook.com>
2019-05-17 19:45:57 -0300

    Made some changes to my prompt. Looks way better now

--- Profile.ps1
+++ Profile.ps1
@@ -1,3 +1,4 @@
+# Prompt based on https://bradwilson.io/blog/prompt/powershell
 $profileDir = Split-Path $profile
 
 Import-Module posh-alias
@@ -6,32 +7,125 @@ Import-Module "$profileDir/Modules/posh-docker/posh-docker/posh-docker.psd1"
 Import-Module "$profileDir/Modules/z/z.psm1"
 
 $setPwd = $false
-if(($env:PSLastLocation -ne $null) -and (Test-Path $env:PSLastLocation -PathType Container)) {
+if (($env:PSLastLocation -ne $null) -and (Test-Path $env:PSLastLocation -PathType Container)) {
 	$setPwd = $true
 }
 
-function Prompt {
-	if($setPwd -eq $true) {
+set-content Function:prompt {
+	if ($setPwd -eq $true) {
 		Set-Location $env:PSLastLocation
 		$Script:setPwd = $false
 	}
 
-	try {
-		$gitBranch = (git branch | Where-Object { $_.startsWith('*') }).TrimStart('* ') 2> $null
-	}
-	catch {
-		$gitBranch = $null;
+	# Start with a blank line, for breathing room :)
+	Write-Host ""
+
+	# Reset the foreground color to default
+	$Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultColor.ForegroundColor
+
+	# Write ERR for any PowerShell errors
+	if ($Error.Count -ne 0) {
+		Write-Host " " -NoNewLine
+		Write-Host "  ERR " -NoNewLine -BackgroundColor DarkRed -ForegroundColor Yellow
+		$Error.Clear()
 	}
 
-	if($gitBranch -ne $null -and $gitBranch -ne "") {
-		$gitBranch = " ($gitBranch)"
+	# Write non-zero exit code from last launched process
+	if ($LASTEXITCODE -ne "") {
+		Write-Host " " -NoNewLine
+		Write-Host "  $LASTEXITCODE " -NoNewLine -BackgroundColor DarkRed -ForegroundColor Yellow
+		$LASTEXITCODE = ""
 	}
 
+	# Write any custom prompt environment (f.e., from vs2017.ps1)
+	if (get-content variable:\PromptEnvironment -ErrorAction Ignore) {
+		Write-Host " " -NoNewLine
+		Write-Host $PromptEnvironment -NoNewLine -BackgroundColor DarkMagenta -ForegroundColor White
+	}
+
+	# Write the current kubectl context
+	if ($env:PS_KUBECTL -NE $false -And (Get-Command "kubectl" -ErrorAction Ignore) -ne $null) {
+		$currentContext = (& kubectl config current-context 2> $null)
+		if ($Error.Count -eq 0) {
+			Write-Host " " -NoNewLine
+			Write-Host " " -NoNewLine -BackgroundColor DarkGray -ForegroundColor Green
+			Write-Host " $currentContext " -NoNewLine -BackgroundColor DarkGray -ForegroundColor White
+		}
+		else {
+			===
+			$Error.Clear()
+		}
+	}
+
+	# Write the current public cloud Azure CLI subscription
+	# NOTE: You will need sed from somewhere (for example, from Git for Windows)
+	# if (Test-Path ~/.azure/clouds.config) {
+	# 	$currentSub = & sed -nr "/^\[AzureCloud\]/ { :l /^subscription[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ~/.azure/clouds.config
+	# 	if ($null -ne $currentSub) {
+	# 		$currentAccount = (Get-Content ~/.azure/azureProfile.json | ConvertFrom-Json).subscriptions | Where-Object { $_.id -eq $currentSub }
+	# 		if ($null -ne $currentAccount) {
+	# 			Write-Host " " -NoNewLine
+	# 			Write-Host " " -NoNewLine -BackgroundColor DarkCyan -ForegroundColor Yellow
+	# 			Write-Host " $($currentAccount.name) " -NoNewLine -BackgroundColor DarkCyan -ForegroundColor White
+	# 		}
+	# 	}
+	# }
+
+	# Write the current Git information
+	if ((Get-Command "Get-GitDirectory" -ErrorAction Ignore) -ne $null) {
+		if (Get-GitDirectory -ne $null) {
+			Write-Host (Write-VcsStatus) -NoNewLine
+		}
+	}
+
+	# Write the current directory, with home folder normalized to ~
+	$currentPath = (get-location).Path.replace($home, "~")
+	$idx = $currentPath.IndexOf("::")
+	if ($idx -gt -1) { $currentPath = $currentPath.Substring($idx + 2) }
+
+	Write-Host " " -NoNewLine
+	Write-Host " " -NoNewLine -BackgroundColor DarkGreen -ForegroundColor Yellow
+	Write-Host " $currentPath " -NoNewLine -BackgroundColor DarkGreen -ForegroundColor White
+
+	# Reset LASTEXITCODE so we don't show it over and over again
+	$global:LASTEXITCODE = 0
+
+	# Write one + for each level of the pushd stack
+	if ((get-location -stack).Count -gt 0) {
+		Write-Host " " -NoNewLine
+		Write-Host (("+" * ((get-location -stack).Count))) -NoNewLine -ForegroundColor Cyan
+	}
+
+	# Newline
+	Write-Host ""
+
+	# Determine if the user is admin, so we color the prompt green or red
+	$isAdmin = $false
+	$isDesktop = ($PSVersionTable.PSEdition -eq "Desktop")
+
+	if ($isDesktop -or $IsWindows) {
+		$windowsIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+		$windowsPrincipal = new-object 'System.Security.Principal.WindowsPrincipal' $windowsIdentity
+		$isAdmin = $windowsPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator) -eq 1
+	}
+ else {
+		$isAdmin = ((& id -u) -eq 0)
+	}
+
+	if ($isAdmin) { $color = "Red"; }
+	else { $color = "Green"; }
+
+	# Write PS> for desktop PowerShell, pwsh> for PowerShell Core
+	if ($isDesktop) {
+		Write-Host " PS>" -NoNewLine -ForegroundColor $color
+	}
+	else {
+		Write-Host " pwsh>" -NoNewLine -ForegroundColor $color
+	}
+
+	# Update the env, so that ConEmu can restart the current console if needed
 	$location = Get-Location
 	$pwd = $location.Path
-
-	$env:PSLastLocation = $pwd
-
 	if($location.Drive.Provider.Name -eq "FileSystem") {
 		[system.environment]::currentdirectory = $pwd
 	} elseif($pwd.Contains("::")) {
@@ -39,19 +133,8 @@ function Prompt {
 		[system.environment]::currentdirectory = $pwd
 	}
 
-	$pwd = $pwd.Replace($env:userprofile, "~");
-
-	Write-Host "`nPS " -foreground Gray -nonewline
-	Write-Host $pwd -foreground Green -nonewline
-	Write-Host $gitBranch -foreground DarkGray -NoNewline
-	Write-Host ">" -foreground Gray -nonewline
-	" "
-}
-
-function use-git-unix {
-	function global:git {
-		bash.exe -c "git $args"
-	}
+	# Always have to return something or else we get the default prompt
+	return " "
 }
 
 Add-Alias status 'git status'
@@ -59,12 +142,16 @@ Add-Alias pull 'git pull -pt --all'
 Add-Alias push 'git push -u'
 Add-Alias fetch 'git fetch -pt --all'
 Add-Alias branch 'git branch'
+Add-Alias gk 'gitk --all'
+Add-Alias d docker
+Add-Alias dc docker-compose
+Add-Alias k kubectl
 
 Add-Alias http 'Invoke-WebRequest -UseBasicParsing'
 
 function sync {
 	pull
-	if($lastexitcode -eq 0) {
+	if ($lastexitcode -eq 0) {
 		push
 	}
 }
@@ -75,27 +162,28 @@ function upstream([string]$branch) {
 
 function mklink([string]$link, [string]$target) {
 	$exists = Test-Path $target
-	if($exists) {
+	if ($exists) {
 		New-Item $link -ItemType SymbolicLink -Value $target | out-null
 		return $link
 	}
 }
 
 function hardlink([string]$link, [string]$target) {
-	if(Test-Path $target -PathType Leaf) {
+	if (Test-Path $target -PathType Leaf) {
 		New-Item $link -ItemType HardLink -Value $target | out-null
 		return $link
-	} elseif(Test-Path $target -PathType Container) {
+	}
+ elseif (Test-Path $target -PathType Container) {
 		New-Item $link -ItemType Junction -Value $target | out-null
 		return $link
 	}
 }
 
 function Find-Command([Parameter(
-	Position=0,
-	Mandatory=$true,
-	ValueFromPipeline=$true,
-	ValueFromPipelineByPropertyName=$true)]
+		Position = 0,
+		Mandatory = $true,
+		ValueFromPipeline = $true,
+		ValueFromPipelineByPropertyName = $true)]
 	[String]$command) {
 
 	return Get-Command $command | Split-Path
@@ -103,17 +191,18 @@ function Find-Command([Parameter(
 
 function Open(
 	[Parameter(
-			Position=0,
-		ValueFromPipeline=$true,
-		ValueFromPipelineByPropertyName=$true)]
+		Position = 0,
+		ValueFromPipeline = $true,
+		ValueFromPipelineByPropertyName = $true)]
 	[String]$path) {
-	if([system.string]::IsNullOrWhiteSpace($path)){
+	if ([system.string]::IsNullOrWhiteSpace($path)) {
 		explorer .
 		return;
 	}
-	if(Test-Path $path -PathType Leaf) {
+	if (Test-Path $path -PathType Leaf) {
 		$path
-	} elseif(Test-Path $path -PathType Container) {
+	}
+ elseif (Test-Path $path -PathType Container) {
 		explorer $path
 	}
 }
@@ -131,18 +220,8 @@ function run-bash() {
 	bash -c $($args -join ' ')
 }
 
-# function git() {
-# 	# if($env:useBash -eq $true) {
-# 	run-bash "git" @args;
-# 	# } else {
-# 	# 	git.exe @args
-# 	# }
-# }
-
 function Erase-History() {
 	Clear-History | Out-Null
-	#[system.reflection.assembly]::loadwithpartialname("System.Windows.Forms") | Out-Null
-	#[System.Windows.Forms.SendKeys]::Sendwait('%{F7 2}') | Out-Null
 	Remove-Item (Get-PSReadlineOption).HistorySavePath | Out-Null
 	[Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory() | Out-Null
 	Clear-Host
@@ -153,3 +232,55 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
 	Import-Module "$ChocolateyProfile"
 }
+
+# Background colors
+
+$GitPromptSettings.AfterStash.BackgroundColor = [ConsoleColor]::DarkBlue
+$GitPromptSettings.AfterStatus.BackgroundColor = [ConsoleColor]::DarkBlue
+$GitPromptSettings.BeforeIndex.BackgroundColor = [ConsoleColor]::DarkBlue
+$GitPromptSettings.BeforeStash.BackgroundColor = [ConsoleColor]::DarkBlue
+$GitPromptSettings.BeforeStatus.BackgroundColor = [ConsoleColor]::DarkBlue
+$GitPromptSettings.BranchAheadStatusSymbol.BackgroundColor = [ConsoleColor]::DarkBlue
+$GitPromptSettings.BranchBehindAndAheadStatusSymbol.BackgroundColor = [ConsoleColor]::DarkBlue
+$GitPromptSettings.BranchBehindStatusSymbol.BackgroundColor = [ConsoleColor]::DarkBlue
+$GitPromptSettings.BranchColor.BackgroundColor = [ConsoleColor]::DarkBlue
+$GitPromptSettings.BranchGoneStatusSymbol.BackgroundColor = [ConsoleColor]::DarkBlue
+$GitPromptSettings.BranchIdenticalStatusSymbol.BackgroundColor = [ConsoleColor]::DarkBlue
+$GitPromptSettings.DefaultColor.BackgroundColor = [ConsoleColor]::DarkBlue
+$GitPromptSettings.DelimStatus.BackgroundColor = [ConsoleColor]::DarkBlue
+$GitPromptSettings.ErrorColor.BackgroundColor = [ConsoleColor]::DarkBlue
+$GitPromptSettings.IndexColor.BackgroundColor = [ConsoleColor]::DarkBlue
+$GitPromptSettings.LocalDefaultStatusSymbol.BackgroundColor = [ConsoleColor]::DarkBlue
+$GitPromptSettings.LocalStagedStatusSymbol.BackgroundColor = [ConsoleColor]::DarkBlue
+$GitPromptSettings.LocalWorkingStatusSymbol.BackgroundColor = [ConsoleColor]::DarkBlue
+$GitPromptSettings.StashColor.BackgroundColor = [ConsoleColor]::DarkBlue
+$GitPromptSettings.WorkingColor.BackgroundColor = [ConsoleColor]::DarkBlue
+
+# Foreground colors
+
+$GitPromptSettings.AfterStatus.ForegroundColor = [ConsoleColor]::Blue
+$GitPromptSettings.BeforeStatus.ForegroundColor = [ConsoleColor]::Blue
+$GitPromptSettings.BranchColor.ForegroundColor = [ConsoleColor]::White
+$GitPromptSettings.BranchGoneStatusSymbol.ForegroundColor = [ConsoleColor]::Blue
+$GitPromptSettings.BranchIdenticalStatusSymbol.ForegroundColor = [ConsoleColor]::Blue
+$GitPromptSettings.DefaultColor.ForegroundColor = [ConsoleColor]::Gray
+$GitPromptSettings.DelimStatus.ForegroundColor = [ConsoleColor]::Blue
+$GitPromptSettings.IndexColor.ForegroundColor = [ConsoleColor]::Cyan
+$GitPromptSettings.WorkingColor.ForegroundColor = [ConsoleColor]::Yellow
+
+# Prompt shape
+
+$GitPromptSettings.AfterStatus.Text = " "
+$GitPromptSettings.BeforeStatus.Text = "  "
+$GitPromptSettings.BranchAheadStatusSymbol.Text = ""
+$GitPromptSettings.BranchBehindStatusSymbol.Text = ""
+$GitPromptSettings.BranchGoneStatusSymbol.Text = ""
+$GitPromptSettings.BranchBehindAndAheadStatusSymbol.Text = ""
+$GitPromptSettings.BranchIdenticalStatusSymbol.Text = ""
+$GitPromptSettings.BranchUntrackedText = "※ "
+$GitPromptSettings.DelimStatus.Text = " ॥"
+$GitPromptSettings.LocalStagedStatusSymbol.Text = ""
+$GitPromptSettings.LocalWorkingStatusSymbol.Text = ""
+
+$GitPromptSettings.EnableStashStatus = $false
+$GitPromptSettings.ShowStatusWhenZero = $false
